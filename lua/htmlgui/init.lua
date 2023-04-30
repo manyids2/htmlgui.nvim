9f9fe92cf68db98f8ddb2177b9ef2ce99fcd802a
manyids2 <arun.mukundan@gmail.com>
2023-04-30 07:14:19 +0200

    Proof of concept with hot reloading

--- lua/htmlgui/init.lua
+++ lua/htmlgui/init.lua
@@ -2,7 +2,58 @@ function P(x)
 	print(vim.inspect(x))
 end
 
-local ts_html = require("htmlgui.ts_html")
+local a = vim.api
+local ts = vim.treesitter
+local h = require("htmlgui.ts_html")
 
-ts_html.init()
+h.init()
 
+local function get_rect_from_div(div)
+	local parts = vim.split(div.attrs.style, ";")
+	local rect = {}
+	for _, v in ipairs(parts) do
+		local vv = vim.split(v, ":")
+		if vim.tbl_count(vv) == 2 then
+			rect[vim.trim(vv[1])] = tonumber(vim.trim(vv[2]))
+		end
+	end
+	return rect
+end
+
+local function parse_div(node, buf)
+	local tag = node:child()
+	local nattrs = tag:named_child_count() -- start_tag
+	local attrs = {}
+	for i = 1, nattrs - 1 do
+		local attr = tag:named_child(i)
+		local attr_key = ts.get_node_text(attr:named_child(0), buf)
+		local attr_value = ts.get_node_text(attr:named_child(1):named_child(0), buf)
+		attrs[attr_key] = attr_value
+	end
+	local text = ts.get_node_text(tag:next_named_sibling(), buf)
+	return { tag = "div", attrs = attrs, text = text }
+end
+
+local function render()
+  for _, s in ipairs(h.state.data) do
+    a.nvim_win_close(s.win, true)
+  end
+  h.state.data = {}
+	local body = h.get_body(h.state.html.buf)
+	for i = 2, vim.tbl_count(body:named_children()) - 1 do
+		local child = body:named_children()[i]
+		local div = parse_div(child, h.state.html.buf)
+		local rect = get_rect_from_div(div)
+    table.insert(h.state.data, h.create_win(rect))
+	end
+end
+
+-- Highlight on yank
+local au_save = a.nvim_create_augroup("htmlgui_save", { clear = true })
+a.nvim_create_autocmd("BufWritePost", {
+	group = au_save,
+	pattern = { "*.html" },
+	callback = function()
+		render()
+	end,
+})
--- lua/htmlgui/ts_html.lua
+++ lua/htmlgui/ts_html.lua
@@ -1,3 +1,7 @@
+function P(x)
+	print(vim.inspect(x))
+end
+
 local M = {}
 M.state = {}
 
@@ -18,13 +22,12 @@ function M.init()
 	local gui_buf = a.nvim_create_buf(false, true)
 	a.nvim_win_set_buf(gui_win, gui_buf)
 
-	local parser = parsers.get_parser(html_buf, "html")
-	local root = parser:parse()[1]:root()
-
+	local root = M.get_root(html_buf)
 	M.state = {
 		html = { win = html_win, buf = html_buf },
 		gui = { win = gui_win, buf = gui_buf },
 		root = root,
+    data = {}
 	}
 
 	local filename = vim.fs.basename(a.nvim_buf_get_name(html_buf))
@@ -32,6 +35,12 @@ function M.init()
 	local script = M.get_script(root, html_buf)
 	local style = M.get_style(root, html_buf)
 	vim.opt.statusline = M.status_line(gui_win, title, filename, script, style)
+end
+
+function M.get_root(buf)
+	local parser = parsers.get_parser(buf, "html")
+	local root = parser:parse()[1]:root()
+	return root
 end
 
 function M.status_line(win, title, filename, script, style)
@@ -59,10 +68,22 @@ function M.get_first_match(query, root, buf)
 	end
 end
 
-function M.get_text_from_metadata(metadata, buf)
-	local r = metadata.range
-	local m = { sr = r[1], sc = r[2], er = r[3], ec = r[4] }
-	local text = a.nvim_buf_get_text(buf, m.sr, m.sc, m.er, m.ec, {})[1]
+function M.get_all_matches(query, root, buf)
+	local parsed_query = ts.query.parse("html", query)
+	local s, _, e, _ = root:range()
+	local matches = {}
+	for _, node, metadata in parsed_query:iter_captures(root, buf, s, e) do
+		local ids = vim.tbl_keys(metadata)
+		if vim.tbl_count(ids) > 0 then
+			metadata = metadata[ids[1]]
+		end
+		table.insert(matches, { node = node, metadata = metadata })
+	end
+	return matches
+end
+
+function M.get_text_from_range(range, buf)
+	local text = a.nvim_buf_get_text(buf, range[1], range[2], range[3], range[4], {})[1]
 	return text
 end
 
@@ -75,7 +96,7 @@ function M.get_title(root, buf)
       (text) @_text (#offset! @_text 0 0 0 0 ))
   )]]
 	local tt = M.get_first_match(query, root, buf)
-	return M.get_text_from_metadata(tt.metadata, buf)
+	return M.get_text_from_range(tt.metadata.range, buf)
 end
 
 function M.get_style(root, buf)
@@ -93,7 +114,7 @@ function M.get_style(root, buf)
   (#offset! @_value 0 0 0 0 )))
   )]]
 	local tt = M.get_first_match(query, root, buf)
-	return M.get_text_from_metadata(tt.metadata, buf)
+	return M.get_text_from_range(tt.metadata.range, buf)
 end
 
 function M.get_script(root, buf)
@@ -106,7 +127,77 @@ function M.get_script(root, buf)
   (#offset! @_value 0 0 0 0 )
 )]]
 	local tt = M.get_first_match(query, root, buf)
-	return M.get_text_from_metadata(tt.metadata, buf)
+	return M.get_text_from_range(tt.metadata.range, buf)
+end
+
+function M.get_body(buf)
+	local root = M.get_root(buf)
+	local query = [[(
+  element
+    (start_tag (tag_name) @_body)
+    (#eq? @_body "body")
+)]]
+	local tt = M.get_first_match(query, root, buf)
+	return tt.node:parent():parent()
+end
+
+function M.get_divs(root, buf)
+	local query = [[(
+  element
+    (start_tag (tag_name) @_div)
+    (#eq? @_div "div")
+)]]
+	local tt = M.get_all_matches(query, root, buf)
+	return tt
+end
+
+function M.get_width_height()
+	local stats = a.nvim_list_uis()[1]
+	local width = stats.width
+	local height = stats.height
+	return { width = width, height = height }
+end
+
+function M.get_centre_rect(fraction)
+	local size = M.get_width_height()
+	local width = math.ceil(size.width * fraction)
+	local height = math.ceil(size.height * fraction)
+	local col = math.ceil((size.width - width) / 2)
+	local row = math.ceil((size.height - height) / 2) - 1
+	return { col = col, row = row, width = width, height = height }
+end
+
+function M.create_win(rect)
+	local buf = a.nvim_create_buf(false, true)
+	local size = M.get_width_height()
+	if rect.col < 1 then
+		rect.col = math.ceil(size.width * rect.col)
+	end
+	if rect.row < 1 then
+		rect.row = math.ceil(size.height * rect.row)
+	end
+	if rect.height < 1 then
+		rect.height = math.ceil(size.height * rect.height)
+	end
+	if rect.width < 1 then
+		rect.width = math.ceil(size.width * rect.width)
+	end
+
+	if rect.zindex == nil then
+		rect.zindex = 10
+	end
+
+	local opts = {
+		relative = "editor",
+		col = rect.col,
+		row = rect.row,
+		width = rect.width,
+		height = rect.height,
+		zindex = rect.zindex,
+		style = "minimal",
+	}
+	local win = a.nvim_open_win(buf, true, opts)
+	return { win = win, buf = buf }
 end
 
 return M
