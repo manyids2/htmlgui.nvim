802387296401a310d0834d755ad696a14ac23c25
manyids2 <arun.mukundan@gmail.com>
2023-05-09 22:17:27 +0200

    Need to add reference by id

--- examples/discount-lazygit/index.lua
+++ examples/discount-lazygit/index.lua
@@ -1,3 +1,23 @@
 local M = {}
 
+function M.preview_status()
+  P("preview_status")
+end
+
+function M.preview_file()
+  P("preview_file")
+end
+
+function M.preview_branch()
+  P("preview_branch")
+end
+
+function M.preview_commit()
+  P("preview_commit")
+end
+
+function M.preview_stash()
+  P("preview_stash")
+end
+
 return M
